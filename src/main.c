ad61f45fca9a83ea344a5e1a3ca2637ac92491d2
Humanity_Three <U202413419@hust.edu.cn>
2026-08-23 14:51:13 +0800

    fix: configure UTF-8 console and source encoding

--- src/main.c
+++ src/main.c
@@ -5,6 +5,10 @@
 
 #include <stdio.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "game.h"
 #include "manual_ui.h"
 #include "player_setup.h"
@@ -13,6 +17,12 @@ int main(void)
 {
     Game game;
     PlayerSetupStatus status;
+
+#ifdef _WIN32
+    /* 玩家设置在 manual_ui 之前输出，因此必须在程序入口切换 UTF-8。 */
+    (void)SetConsoleOutputCP(CP_UTF8);
+    (void)SetConsoleCP(CP_UTF8);
+#endif
 
     game_init(&game);
     if (game_load_map(&game, "map.json") != RC_OK) {
